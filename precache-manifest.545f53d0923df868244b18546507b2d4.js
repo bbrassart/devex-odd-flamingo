c5c850e5bcfaeaf98093b1ce925f4c95f888f514
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-11-26 15:33:51 +0100

    Updates

---
+++ precache-manifest.545f53d0923df868244b18546507b2d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f719609eb7f2d4fcd1409ec99245f52a",
+    "url": "/devex-odd-flamingo/index.html"
+  },
+  {
+    "revision": "51679ab7a2c453078254",
+    "url": "/devex-odd-flamingo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ca253bd5bd2888dad9a0",
+    "url": "/devex-odd-flamingo/static/css/main.3d4fcbb5.chunk.css"
+  },
+  {
+    "revision": "51679ab7a2c453078254",
+    "url": "/devex-odd-flamingo/static/js/2.c2fefd98.chunk.js"
+  },
+  {
+    "revision": "ca253bd5bd2888dad9a0",
+    "url": "/devex-odd-flamingo/static/js/main.a71df634.chunk.js"
+  },
+  {
+    "revision": "04411e68df7f5e8c7bbb",
+    "url": "/devex-odd-flamingo/static/js/runtime-main.2140a4f0.js"
+  },
+  {
+    "revision": "394a052ee999010f9777ab00e41f6fa4",
+    "url": "/devex-odd-flamingo/static/media/marker.394a052e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71727d1c97302b333d38dca34897fab3.js"
+  "/devex-odd-flamingo/precache-manifest.545f53d0923df868244b18546507b2d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/devex-odd-flamingo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
