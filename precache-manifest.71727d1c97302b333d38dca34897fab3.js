18030960f2ae8d58884751828c27b956624e380f
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-11-26 15:30:15 +0100

    Updates

---
+++ precache-manifest.71727d1c97302b333d38dca34897fab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c6aa1e3eee5ebb2f73125c3caea473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93ff424b68d56e43f13",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8618c5cd0080cbf53cec",
+    "url": "/static/css/main.3d4fcbb5.chunk.css"
+  },
+  {
+    "revision": "c93ff424b68d56e43f13",
+    "url": "/static/js/2.42386b93.chunk.js"
+  },
+  {
+    "revision": "8618c5cd0080cbf53cec",
+    "url": "/static/js/main.e19b5f6b.chunk.js"
+  },
+  {
+    "revision": "83db87dd18b93886fd50",
+    "url": "/static/js/runtime-main.4d3e2383.js"
+  },
+  {
+    "revision": "394a052ee999010f9777ab00e41f6fa4",
+    "url": "/static/media/marker.394a052e.svg"
+  }
+]);
