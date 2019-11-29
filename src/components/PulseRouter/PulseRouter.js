b7d5e1757c1ffc781c3f304e69df9650da83ba9b
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-11-29 11:31:05 +0100

    Odd Flamingo: Start creating link form

--- src/components/PulseRouter/PulseRouter.js
+++ src/components/PulseRouter/PulseRouter.js
@@ -14,7 +14,7 @@ class PulseRouter extends Component {
   }
 
   getUrlParam() {
-    const urlAsArray = window.location.href.split('?/url=');
+    const urlAsArray = window.location.href.split('/url=');
     return urlAsArray[1] ? urlAsArray[1] : null
   }
 
